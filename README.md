63a415bc2d8be49ba7c206b9f9b6f4b446563da8
TechRancher <sikes.jay@gmail.com>
2019-11-28 10:23:41 -0600

    Push To Git

---
+++ README.md
@@ -0,0 +1,6 @@
+jbloks mod
+
+I made this mod to have more realistic type of nodes to help with your building. 
+The node images are 128px so they are pretty go quality.  
+
+The init.lua code has been coded with stairsplus to make the nodes useable with the circular_saw code in [moreblocks](https://github.com/VanessaE/moreblocks). So, in order to take advantage of the additional nodes you must have moreblocks installed.
---
+++ init.lua
@@ -0,0 +1,500 @@
+-- Register Nodes
+    -- Light Color Concrete
+minetest.register_node("jbloks:colorConcrete", {
+    description = "Light Color Concrete ",
+    tiles = {"jbloks_colorConcrete.jpg" },
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=2},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+    -- Lite Bolted Wooden Planks
+minetest.register_node("jbloks:boltedLiteWoodenPlanks", {
+    description = "Lite Wood Color Plank ",
+    tiles = {"jbloks_boltedLiteWoodenPlanks.jpg" },
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+    -- Bricks Red
+minetest.register_node("jbloks:bricksRed", {
+    description = "Red Brick ",
+    tiles = {"jbloks_bricksRed.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+    -- Bricks White
+minetest.register_node("jbloks:bricksWhite", {
+    description = "White Bricks ",
+    tiles = {"jbloks_bricksWhite.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Diamond Plate
+minetest.register_node("jbloks:diamondPlate", {
+    description = "Diamond Plate ",
+    tiles = {"jbloks_diamondPlate.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Flat Brick Red
+minetest.register_node("jbloks:flatBrickRed", {
+    description = "Flat Brick Red ",
+    tiles = {"jbloks_flatBrickRed.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Herringbone Brick Gray
+minetest.register_node("jbloks:herringboneBrickGray", {
+    description = "Herringbone Brick Gray ",
+    tiles = {"jbloks_herringboneBrickGray.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Herringbone Brick Red
+minetest.register_node("jbloks:herringboneBrickRed", {
+    description = "Herringbone Brick Red ",
+    tiles = {"jbloks_herringboneBrickRed.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Herringbone Brick White
+minetest.register_node("jbloks:herringboneBrickWhite", {
+    description = "Herringbone Brick White ",
+    tiles = {"jbloks_herringboneBrickWhite.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Metal Aircraft Interior Rust
+minetest.register_node("jbloks:metalAircraftInteriorRusty", {
+    description = "Metal Aircraft Interior Rusty ",
+    tiles = {"jbloks_metalAircraftInteriorRusty.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Metal Galvanized
+minetest.register_node("jbloks:metalGalvanized", {
+    description = "Metal Galvanized ",
+    tiles = {"jbloks_metalGalvanized.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Old Brick
+minetest.register_node("jbloks:oldBrick", {
+    description = "Old Brick ",
+    tiles = {"jbloks_oldBrick.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Red Old Brick
+minetest.register_node("jbloks:oldRedBrick", {
+    description = "Old Red Brick ",
+    tiles = {"jbloks_oldRedBrick.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Old Wood
+minetest.register_node("jbloks:oldWood", {
+    description = "Old Wood ",
+    tiles = {"jbloks_oldWood.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Dark Gray Painted Plaster
+minetest.register_node("jbloks:darkGrayPaintedPlaster", {
+    description = "Dark Gray Painted Plaster ",
+    tiles = {"jbloks_paintedPlasterDarkGray.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Gray Painted Plaster
+minetest.register_node("jbloks:grayPaintedPlaster", {
+    description = "Gray Painted Plaster ",
+    tiles = {"jbloks_paintedPlasterGray.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Ruff Brick
+minetest.register_node("jbloks:ruffBrick", {
+    description = "Ruff Brick ",
+    tiles = {"jbloks_ruffBrick.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Stone Pavement
+minetest.register_node("jbloks:stonePavement", {
+    description = "Stone Pavement ",
+    tiles = {"jbloks_stonePavement.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Tile Gravel
+minetest.register_node("jbloks:tileGravel", {
+    description = "Tile Gravel ",
+    tiles = {"jbloks_tileGravel.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+   -- Weathered Wood
+minetest.register_node("jbloks:weatheredWood", {
+    description = "Weathered Wood ",
+    tiles = {"jbloks_weatheredWood.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Wooden Planks
+minetest.register_node("jbloks:woodenPlanks", {
+    description = "Wooden Planks ",
+    tiles = {"jbloks_woodenPlanks.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Tan Wooden Planks
+minetest.register_node("jbloks:tanWoodenPlanks", {
+    description = "Tan Wooden Planks ",
+    tiles = {"jbloks_woodenPlanksTan.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Wood Floor
+minetest.register_node("jbloks:woodFloor", {
+    description = "Wood Floor ",
+    tiles = {"jbloks_woodFloor.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Brown Painted Wood 
+minetest.register_node("jbloks:brownPaintedWood", {
+    description = "Brown Painted Wood ",
+    tiles = {"jbloks_woodPaintedBrown.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Light Panel Wood 
+minetest.register_node("jbloks:lightPanelWood", {
+    description = "Light Panel Wood ",
+    tiles = {"jbloks_woodPanelLight.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Wood Siding
+minetest.register_node("jbloks:woodSiding", {
+    description = "Wood Siding ",
+    tiles = {"jbloks_woodSiding.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Dark Wood Siding
+minetest.register_node("jbloks:darkWoodSiding", {
+    description = "Dark Wood Siding ",
+    tiles = {"jbloks_woodSidingDark.jpg"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Black Wood Tiles
+minetest.register_node("jbloks:blackWoodTiles", {
+    description = "Black Wood Tiles ",
+    tiles = {"jbloks_woodTilesBlack.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Dark Wood Tiles
+minetest.register_node("jbloks:darkWoodTiles", {
+    description = "Dark Wood Tiles ",
+    tiles = {"jbloks_woodTilesDark.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+   -- Light Wood Tiles
+minetest.register_node("jbloks:lightWoodTiles", {
+    description = "Light Wood Tiles ",
+    tiles = {"jbloks_woodTilesLight.png"},
+    paramtype = "light",
+    paramtype2 = "facedir",
+    groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+    sounds = default.node_sound_wood_defaults(),
+})
+
+-- Stairsplus Registration
+if minetest.get_modpath("moreblocks") then
+    --[[stairsplus:register_all("jbloks", "metalGalvanized", "jbloks:metalGalvanized", {
+        description = "Metal Galvanized ",
+        tiles = {"jbloks_metalGalvanized.jpg" },
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "colorConcrete", "jbloks:colorConcrete", {
+        description = "Light_Color_Concrete ",
+        tiles = {"jbloks_colorConcrete.jpg" },
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=2},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "boltedLiteWoodenPlanks", "jbloks:boltedLiteWoodenPlanks", {
+        description = "Lite Wood Color Plank ",
+        tiles = {"jbloks_boltedLiteWoodenPlanks.jpg" },
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "bricksRed", "jbloks:bricksRed", {
+        description = "Red Brick ",
+        tiles = {"jbloks_bricksRed.jpg"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "diamondPlate", "jbloks:diamondPlate", {
+        description = "Diamond Plate ",
+        tiles = {"jbloks_diamondPlate.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "flatBrickRed", "jbloks:flatBrickRed", {
+        description = "Flat Brick Red ",
+        tiles = {"jbloks_flatBrickRed.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "herringboneBrickGray", "jbloks:herringboneBrickGray", {
+        description = "Herringbone Brick Gray ",
+        tiles = {"jbloks_herringboneBrickGray.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })--]]
+
+    stairsplus:register_all("jbloks", "herringboneBrickRed", "jbloks:herringboneBrickRed", {
+        description = "Herringbone Brick Red ",
+        tiles = {"jbloks_herringboneBrickRed.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })--[[
+
+    stairsplus:register_all("jbloks", "herringboneBrickWhite", "jbloks:herringboneBrickWhite", {
+        description = "Herringbone Brick White ",
+        tiles = {"jbloks_herringboneBrickWhite.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "metalAircraftInteriorRusty", "jbloks:metalAircraftInteriorRusty", {
+        description = "Metal Aircraft Interior Rusty ",
+        tiles = {"jbloks_metalAircraftInteriorRusty.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "oldBrick", "jbloks:oldBrick", {
+        description = "Old Brick ",
+        tiles = {"jbloks_oldBrick.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "oldRedBrick", "jbloks:oldRedBrick", {
+        description = "Old Red Brick ",
+        tiles = {"jbloks_oldRedBrick.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })--]]
+
+    stairsplus:register_all("jbloks", "oldWood", "jbloks:oldWood", {
+        description = "Old Wood ",
+        tiles = {"jbloks_oldWood.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    --[[stairsplus:register_all("jbloks", "darkGrayPaintedPlaster", "jbloks:darkGrayPaintedPlaster", {
+        description = "Dark Gray Painted Plaster ",
+        tiles = {"jbloks_paintedPlasterDarkGray.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "lightGrayPaintedPlaster", "jbloks:lightGrayPaintedPlaster", {
+        description = "Light Gray Painted Plaster ",
+        tiles = {"jbloks_paintedPlasterLightGray.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "ruffBrick", "jbloks:ruffBrick", {
+        description = "Ruff Brick ",
+        tiles = {"jbloks_ruffBrick.png"},
+        groups = {cracky=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "weatheredWood", "jbloks:weatheredWood", {
+        description = "Weathered Wood ",
+        tiles = {"jbloks_weatheredWood.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "woodenPlanks", "jbloks:woodenPlanks", {
+        description = "Wooden Planks ",
+        tiles = {"jbloks_woodenPlanks.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "tanWoodenPlanks", "jbloks:tanWoodenPlanks", {
+        description = "Tan Wooden Planks ",
+        tiles = {"jbloks_woodenPlanksTan.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "woodFloor", "jbloks:woodFloor", {
+        description = "Wood Floor ",
+        tiles = {"jbloks_woodFloor.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "brownPaintedWood", "jbloks:brownPaintedWood", {
+        description = "Brown Painted Wood ",
+        tiles = {"jbloks_woodPaintedBrown.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "lightPanelWood", "jbloks:lightPanelWood", {
+        description = "Light Panel Wood ",
+        tiles = {"jbloks_woodPanelLight.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "woodSiding", "jbloks:woodSiding", {
+        description = "Wood Siding ",
+        tiles = {"jbloks_woodSiding.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "darkWoodSiding", "jbloks:darkWoodSiding", {
+        description = "Dark Wood Siding ",
+        tiles = {"jbloks_woodSidingDark.jpg"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "blackWoodTiles", "jbloks:blackWoodTiles", {
+        description = "Black Wood Tiles ",
+        tiles = {"jbloks_woodTilesBlack.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "darkWoodTiles", "jbloks:darkWoodTiles", {
+        description = "Dark Wood Tiles ",
+        tiles = {"jbloks_woodTilesDark.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })
+
+    stairsplus:register_all("jbloks", "lightWoodTiles", "jbloks:lightWoodTiles", {
+        description = "Light Wood Tiles ",
+        tiles = {"jbloks_woodTilesLight.png"},
+        groups = {wood=2, oddly_breakable_by_hand=3, choppy=3},
+        sounds = default.node_sound_wood_defaults(),
+    })--]]
+
+end
+
+-- pkarcs nodes
+if minetest.get_modpath("pkarcs") then 
+    pkarcs.register_node("jbloks:bricksRed") 
+    pkarcs.register_node("jbloks:bricksWhite") 
+    pkarcs.register_node("jbloks:flatBrickRed") 
+end
